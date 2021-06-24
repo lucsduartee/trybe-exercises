abd08f93db956787692496db311bb499e87ed6af
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-24 16:38:49 -0300

    adicionado os enunciados nos exercicios

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/arrayfor/exercicio9.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/arrayfor/exercicio9.js
@@ -1,3 +1,5 @@
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
 let array = [];
 for (let i = 1; i <= 25; i+=1) {
   array.push(i);
