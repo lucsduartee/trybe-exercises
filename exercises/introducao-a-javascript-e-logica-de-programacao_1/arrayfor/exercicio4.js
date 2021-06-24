fcab47a680338aee65413ddef4f67fe3054395e8
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-24 15:51:44 -0300

    agora sim, exercicio 4 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/arrayfor/exercicio4.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let number of numbers) {
+  soma+=number;
+};
+let media = soma/numbers.length;
+
+if (media > 20) {
+  console.log(`valor maior que 20`);
+} else {
+  console.log(`valor menor ou igual a 20`);
+}
