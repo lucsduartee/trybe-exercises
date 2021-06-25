11bb27b35b6f6ef6bd87dafe68cde2b8c5aaedab
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-25 15:56:27 -0300

    exercicio 2 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/algoritmos_logica/exercicio2.js
@@ -0,0 +1,10 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
+let str = "*";
+
+if (n > 1) {
+  for (let i = 1; i <= n; i+=1) {
+    console.log(str.repeat(i));
+  }
+}
