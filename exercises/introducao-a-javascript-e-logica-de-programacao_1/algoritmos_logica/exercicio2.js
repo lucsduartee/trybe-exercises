e7a81729af381222602126d801beb6db8f84ce62
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-25 18:41:33 -0300

    exercicio 4 finalizado

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/algoritmos_logica/exercicio2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/algoritmos_logica/exercicio2.js
@@ -1,6 +1,6 @@
 // Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
-let n = 5;
+let n = 10;
 let str = "*";
 
 if (n > 1) {
--- exercises/introducao-a-javascript-e-logica-de-programacao_1/algoritmos_logica/exercicio3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/algoritmos_logica/exercicio3.js
@@ -1,4 +1,4 @@
-let n = 5;
+let n = 10;
 let str = "*";
 
 if (n > 1) {
