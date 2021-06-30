5da2d89a761526311bede98556ee1fc085c5cf2d
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-30 17:48:39 -0300

    completed exercise

--- exercises/javascript-dom/5.1/script.js
+++ exercises/javascript-dom/5.1/script.js
@@ -1,7 +1,18 @@
 document.getElementById("header-container").style.backgroundColor = "green";
+
 var colorPink = document.getElementsByClassName("emergency-tasks");
 colorPink[0].style.backgroundColor = "pink"; 
 var textColorPink = document.querySelectorAll(".emergency-tasks h3");
 for (let i = 0; i < textColorPink.length; i += 1) {
   textColorPink[i].style.backgroundColor = "#cc0066";
-}
+};
+
+var colorBlack = document.getElementsByClassName("no-emergency-tasks");
+colorBlack[0].style.backgroundColor = "#ffff00";
+var textColorBlack = document.querySelectorAll(".no-emergency-tasks h3");
+for (let i = 0; i < textColorPink.length; i += 1) {
+  textColorBlack[i].style.backgroundColor = "black";
+};
+
+var colorFooter = document.getElementById("footer-container");
+colorFooter.style.backgroundColor = "#00452d";
