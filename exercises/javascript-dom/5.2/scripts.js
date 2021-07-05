93d2304b99166b39c77aef39fde2daa5400c6a5a
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-05 16:03:53 -0300

    exercicios da aula terminado

---
+++ exercises/javascript-dom/5.2/scripts.js
@@ -0,0 +1,26 @@
+// Acesse o elemento elementoOndeVoceEsta .
+var elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+console.log(elementoOndeVoceEsta);
+
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+elementoOndeVoceEsta.parentNode.style.backgroundColor = "#CCC";
+
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
+elementoOndeVoceEsta.firstElementChild.innerText = "Texto dentro do primeiroFilhoDoFilho";
+
+// Acesse o primeiroFilho a partir de pai.
+var pai = document.getElementById('pai');
+console.log(pai.firstElementChild);
+
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+console.log(elementoOndeVoceEsta.previousElementSibling);
+
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+console.log(elementoOndeVoceEsta.nextSibling);
+
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+var terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+console.log(terceiroFilho);
+
+// Agora acesse o terceiroFilho a partir de pai.
+console.log(pai.lastElementChild.previousElementSibling);
