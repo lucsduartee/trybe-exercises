0cfb279b3d24fb84da82d124116bbceb9901dc99
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-14 17:05:07 -0300

    inserindo campo de telefone

--- exercises/html-forms-css-flexbox/dia-1-html-css-forms/script.js
+++ exercises/html-forms-css-flexbox/dia-1-html-css-forms/script.js
@@ -12,7 +12,7 @@ document.body.insertBefore(mainContent, script);
 const form = document.createElement('form');
 const fieldset = document.createElement('fieldset');
 const legend = document.createElement('legend');
-legend.innerHTML = 'Dados para cadastro'
+legend.innerHTML = 'Dados Pessoais';
 mainContent.appendChild(form);
 form.appendChild(fieldset);
 form.action = '';
@@ -23,6 +23,7 @@ const inputName = document.createElement('input');
 inputName.type = 'text';
 inputName.id = 'nome';
 inputName.maxLength = '40';
+inputName.required = true;
 const labelNome = document.createElement('label');
 labelNome.htmlFor = 'nome';
 labelNome.innerHTML = 'Nome';
@@ -30,14 +31,26 @@ fieldset.appendChild(labelNome);
 fieldset.appendChild(inputName);
 
 const inputEmail = document.createElement('input');
-inputEmail.type = 'text';
+inputEmail.type = 'email';
 inputEmail.id = 'email';
 inputEmail.maxLength = '50';
+inputEmail.required = true;
 const labelEmail = document.createElement('label');
 labelEmail.htmlFor = 'email';
 labelEmail.innerHTML = 'Email';
 fieldset.appendChild(labelEmail);
 fieldset.appendChild(inputEmail);
+
+const inputTelefone = document.createElement('input');
+inputTelefone.type = 'tel';
+inputTelefone.id = 'telefone';
+inputTelefone.maxLength = '11';
+inputTelefone.required = true;
+const labelTelefone = document.createElement('label');
+labelTelefone.htmlFor = 'telefone';
+labelTelefone.innerHTML = 'Telefone';
+fieldset.appendChild(labelTelefone);
+fieldset.appendChild(inputTelefone);
 
 
 
