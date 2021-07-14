cb49cd07e49b882a424ba8321c1ff76feb2e332d
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-14 17:33:22 -0300

    inserindo campo de estados

--- exercises/html-forms-css-flexbox/dia-1-html-css-forms/script.js
+++ exercises/html-forms-css-flexbox/dia-1-html-css-forms/script.js
@@ -52,5 +52,69 @@ labelTelefone.innerHTML = 'Telefone';
 fieldset.appendChild(labelTelefone);
 fieldset.appendChild(inputTelefone);
 
+const inputEndereco = document.createElement('input');
+inputEndereco.type = 'text';
+inputEndereco.id = 'endereco';
+inputEndereco.maxLength = '200';
+inputEndereco.required = true;
+const labelEndereco = document.createElement('label');
+labelEndereco.htmlFor = 'endereco';
+labelEndereco.innerHTML = 'Endereco';
+fieldset.appendChild(labelEndereco);
+fieldset.appendChild(inputEndereco);
+
+const inputCidade = document.createElement('input');
+inputCidade.type = 'text';
+inputCidade.id = 'cidade';
+inputCidade.maxLength = '28';
+inputCidade.required = true;
+const labelCidade = document.createElement('label');
+labelCidade.htmlFor = 'cidade';
+labelCidade.innerHTML = 'Cidade';
+fieldset.appendChild(labelCidade);
+fieldset.appendChild(inputCidade);
+
+const selectEstado = document.createElement('select');
+selectEstado.id = 'estado'
+const objEstados = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+}
+for (let sigla in objEstados) {
+  const option = document.createElement('option');
+  option.value = sigla;
+  option.innerHTML = objEstados[sigla];
+  selectEstado.appendChild(option);
+}
+const labelEstado = document.createElement('label');
+labelEstado.htmlFor = 'estado';
+labelEstado.innerHTML = 'Estado';
+fieldset.appendChild(labelEstado);
+fieldset.appendChild(selectEstado);
 
 
