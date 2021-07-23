379a8bbbd3e67fe36d0fe934eb5394243a9eaa2e
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-23 18:31:11 -0300

    exercicio 3 main2 finalizado

--- exercises/javascriptES6-testes-unitários/dia-2-javascript-es6-objects/main2.js
+++ exercises/javascriptES6-testes-unitários/dia-2-javascript-es6-objects/main2.js
@@ -27,16 +27,18 @@ console.log(lesson2);
 /*2-Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.*/
 
 const listKeys = obj => console.log(Object.keys(obj))
-listKeys(lesson3);
 
-/*3-Crie uma função para mostrar o tamanho de um objeto.
+/*3-Crie uma função para mostrar o tamanho de um objeto.*/
 
-4-Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+const lenghtObj = obj => console.log(Object.keys(obj).length)
+lenghtObj(lesson2);
 
-5-Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .
+/*4-Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.*/
 
-6-Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+/*5-Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .*/
 
-7-Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. 
+/*6-Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.*/
 
-8-Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.*/
+/*7-Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. */
+
+/*8-Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.*/
