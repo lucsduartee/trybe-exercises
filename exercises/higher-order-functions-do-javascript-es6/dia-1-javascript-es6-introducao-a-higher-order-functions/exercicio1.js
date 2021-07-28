14842054a137402a78211038b7b7a4f72eb0eaf5
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-28 19:57:09 -0300

    declarando numRandom em exercicio1.js e exercicio 2 finalizado

--- exercises/higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
+++ exercises/higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 // Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
 const generateEmail = nomePessoa => {
-  emailPessoa = nomePessoa.replace(/\s/g, '_').toLowerCase();
+  let emailPessoa = nomePessoa.replace(/\s/g, '_').toLowerCase();
   let obj = { 
     nomeCompleto: nomePessoa, 
     email: `${emailPessoa}@trybe.com`,
