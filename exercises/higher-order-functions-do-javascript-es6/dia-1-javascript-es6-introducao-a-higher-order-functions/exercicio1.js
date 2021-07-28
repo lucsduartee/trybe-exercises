af0f161caaeb15fe414ca56e46c6e85d3db1fe09
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-28 19:26:39 -0300

    testes do exercicio 1

--- exercises/higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
+++ exercises/higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 // Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
 const generateEmail = nomePessoa => {
@@ -10,14 +12,19 @@ const generateEmail = nomePessoa => {
   return obj;
 };
 
-const newEmployees = () => {
+const newEmployees = (func) => {
   const employees = {
-    id1: generateEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: generateEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: generateEmail('Carla Paive'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paive'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
-console.log(newEmployees());
+console.log(newEmployees(generateEmail));
+
+// testes 
+assert.strictEqual(typeof newEmployees(generateEmail), 'object');
+assert.strictEqual(typeof generateEmail, 'function');
+
 
