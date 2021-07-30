cd4086b5303af60fe33249dc336102b86fba374a
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-30 15:15:03 -0300

    exercicio 4 finalizado

--- exercises/higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise4.js
+++ exercises/higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise4.js
@@ -64,3 +64,37 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+// Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+const olderBooks = book => (2021 - book.releaseYear) > 60;
+const sortBooks = (bookA, bookB) => bookA.releaseYear - bookB.releaseYear;
+function oldBooksOrdered() {
+  const booksSelected = books.filter(olderBooks);
+  return booksSelected.sort(sortBooks);
+}
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
