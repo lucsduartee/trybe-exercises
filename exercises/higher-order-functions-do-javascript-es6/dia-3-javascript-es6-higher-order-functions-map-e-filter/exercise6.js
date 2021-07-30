3fb4adbc16273f19d5308be276e1c92f972dddb9
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-30 15:53:37 -0300

    exercicio 6 finalizado

--- exercises/higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
+++ exercises/higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
@@ -64,3 +64,19 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu',
+];
+
+//Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+
+
+function oldBooks() {
+  const array = books.filter(book => 2021 - book.releaseYear > 60)
+  return array.map(book => book.name)
+}
+
+assert.deepStrictEqual(oldBooks(), expectedResult);
