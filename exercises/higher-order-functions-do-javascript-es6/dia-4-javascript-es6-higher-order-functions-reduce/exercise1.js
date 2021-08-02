61ebcdec8006847b89da5ae67375f87a736dc0ef
Lucas Duarte <lucs.rkt@gmail.com>
2021-08-02 18:16:36 -0300

    adicionado comentarios no exercicio 1

--- exercises/higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ exercises/higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -8,6 +8,11 @@ const arrays = [
   [4, 5, 6],
 ];
 
+// Callback function que recebe como parâmetro um acumulador, 
+// que nesse caso é um array, e um array que será lido pelo
+// reduce. Nessa função por meio do método push(), o acc
+// recebe o arr desestruturado, o que é possível graças ao
+// Spread Operator. No final a função retorna o acc.
 const toArray = (acc, arr) => {
   acc.push(...arr);
   return acc;
