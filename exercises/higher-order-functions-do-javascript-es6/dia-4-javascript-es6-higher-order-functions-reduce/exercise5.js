c751fad0047e2e643a9986e726df7be7d6383360
Lucas Duarte <lucs.rkt@gmail.com>
2021-08-03 00:49:01 -0300

    exercicio 6 finalizado

--- exercises/higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
+++ exercises/higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -8,7 +8,15 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
+
+//Essa função action recebe como parâmetro um acumlador e um nome, que é uma 
+//string contido no array names. Usando os métodos de string, toUpperCase()
+//para colocar o n4m3 em caixa alta, e depois aplico o método matchAll() pra
+//encontrar todas as ocorrẽncias da letra "A" na string.
+//Depos unsando o spreadOperator eu desestruturo a string em um array contendo as letras
+//"A". Com isso posso saber o comprimento do array e adicionar esse valor no acc.
 const action = (acc, n4m3) => {
+  //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
   let arrOfLetter = [...n4m3.toUpperCase().matchAll('A')];
   return acc += arrOfLetter.length;
 }
@@ -19,3 +27,4 @@ function containsA() {
 
 assert.deepStrictEqual(containsA(), 20);
 
+/* Lucas Duarte*/
