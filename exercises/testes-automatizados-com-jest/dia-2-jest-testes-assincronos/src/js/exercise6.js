b9fc88954839d8c8f763d321c565b70d3e718cb2
Lucas Duarte <lucs.rkt@gmail.com>
2021-08-19 01:11:07 -0300

    exercicio 6 finalizado

---
+++ exercises/testes-automatizados-com-jest/dia-2-jest-testes-assincronos/src/js/exercise6.js
@@ -0,0 +1,17 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animal = Animals.find(animal => animal.name === name);
+      if (animal) return resolve(animal);
+      return reject(new Error('Nenhum animal com esse nome!'));
+    }, 100);
+  })
+);
+
+module.exports = findAnimalByName;
