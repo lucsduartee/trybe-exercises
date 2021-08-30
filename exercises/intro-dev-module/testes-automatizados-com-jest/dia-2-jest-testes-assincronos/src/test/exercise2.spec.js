28372a6ed9275ff065cd5c19b2bb8d9c223dad51
Lucas Duarte <lucs.rkt@gmail.com>
2021-08-30 18:21:37 -0300

    changing folders

--- exercises/intro-dev-module/testes-automatizados-com-jest/dia-2-jest-testes-assincronos/src/test/exercise2.spec.js
+++
@@ -1,9 +0,0 @@
-const getUserName = require("../js/exercise2");
-
-describe("Teste exercício 2", () => {
-  it("teste usuário encontrado", () =>
-    getUserName(1).then((r) => expect(r).toBe('Mark')));
-
-  it("test usuário não encontrado", () =>
-    getUserName(4).catch((e) => expect(e.message).toMatch('User with 4 not found.')));
-});
--- exercises/intro-dev-module/testes-automatizados-com-jest/dia-2-jest-testes-assincronos/src/test/exercise4.spec.js
+++
@@ -1,9 +0,0 @@
-const getRepos = require('../js/exercise4');
-
-describe('Teste exercício 4', () => {
-  it('repositório contém os items', async () => {
-    const items = ['sd-01-week4-5-project-todo-list', 'sd-01-week4-5-project-meme-generator']
-    const response = await getRepos('https://api.github.com/orgs/tryber/repos');
-    expect(response).toEqual(expect.arrayContaining(items));
-  })
-})
