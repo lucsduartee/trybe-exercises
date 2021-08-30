46daf7be115b97471653fb71f8c8be9e50e348ae
Lucas Duarte <lucs.rkt@gmail.com>
2021-08-30 18:50:57 -0300

    exercicio finalizado

--- exercises/front-end-module/bloco-11-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
+++ exercises/front-end-module/bloco-11-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
   );
+};
+
+const arr = ['limpar casa', 'estudar react', 'lavar a roupa', 'assistir série'];
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        {arr.map(item => Task(item))}
+      </>
+    );
+  }
 }
 
 export default App;
