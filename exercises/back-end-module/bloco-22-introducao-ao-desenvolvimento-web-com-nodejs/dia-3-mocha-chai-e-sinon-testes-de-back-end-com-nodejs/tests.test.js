120f5dab346bb83df5cb964e719dd8f0d7053278
Lucas Duarte <lucs.rkt@gmail.com>
2022-01-11 21:49:48 -0300

    feat: finish exercises

--- exercises/back-end-module/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests.test.js
+++ exercises/back-end-module/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests.test.js
@@ -1,6 +1,9 @@
 const { expect } = require('chai');
+const fs = require('fs');
+const sinon = require('sinon');
 
 const { numNaturalFn } = require('./numbers');
+const writeFileFn = require('./writeFileFn');
 
 describe('Executa a função numNaturalFn', () => {
   describe('quando o número for maior que 0', () => {
@@ -38,5 +41,30 @@ describe('Executa a função numNaturalFn', () => {
         expect(answer).to.be.equals('o parâmetro deve ser um número');
       });
     });
+  });
+});
+
+describe('Executa a função writeFileFn', () => {
+  before(() => {
+    sinon.stub(fs, 'writeFileSync');
+  });
+
+  after(() => {
+    fs.writeFileSync.restore();
+  });
+
+  
+  describe('a resposta', () => {
+    it('é uma string', () => {
+      const ans = writeFileFn('arq.txt', 'content');
+
+      expect(ans).to.be.a('string');
+    });
+
+    it('é igual a "ok"', () => {
+      const ans = writeFileFn('arq.txt', 'content');
+
+      expect(ans).to.be.equals('ok');
+    })
   });
 });
