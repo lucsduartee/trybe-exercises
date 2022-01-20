a9b15871b1163a7e436bd20f0c51844642f7106d
Lucas Duarte <lucs.rkt@gmail.com>
2022-01-19 21:20:50 -0300

    feat: created validations

---
+++ exercises/back-end-module/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
+
+
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+})
+
---
+++ exercises/back-end-module/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/middlewares/validations.js
@@ -0,0 +1,45 @@
+const isInvalid = (name) => !name || name === ''; 
+
+const validateName = (req, res, next) => {
+  const { firstName, lastName } = req.body;
+
+  if (isInvalid(firstName) || isInvalid(lastName)) {
+    return res.status(400).json({
+      error: true,
+      message: 'Campos relacionados a nomes são obrigatórios',
+    });
+  }
+
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password || password.length < 6) {
+    return res.status(400).json({
+      error: true,
+      message: 'O campo "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+
+  next();
+}
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const re = /\S+@\S+\.\S+/;
+  if (!re.test(email) || email === '') {
+    return res.status(400).json({
+      error: true,
+      message: 'O campo "email" inválido',
+    });
+  }
+}
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+  validateName,
+};
+
