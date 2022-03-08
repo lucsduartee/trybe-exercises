db259c25b390a639798ef9676ce664118a100b4a
Lucas Duarte <lucs.rkt@gmail.com>
2022-03-07 22:24:50 -0300

    feat(): created routes GET /:id and POST /

--- exercises/back-end-module/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/controllers/Book.js
+++ exercises/back-end-module/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/controllers/Book.js
@@ -10,8 +10,35 @@ const getAll = async (_req, res) => {
 
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-}
+};
+
+const getById = async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.id);
+
+    return res.status(200).json({ book });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const { title, author } = req.body;
+    const created = await Book.create({ title, author });
+
+    return res.status(201).json({ created });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+} 
 
 module.exports = {
   getAll,
+  getById,
+  create,
 }
--- exercises/back-end-module/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/routes/Book.js
+++ exercises/back-end-module/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/routes/Book.js
@@ -4,5 +4,7 @@ const Books = require('../controllers/Book');
 const router = express.Router();
 
 router.get('/', Books.getAll);
+router.get('/:id', Books.getById);
+router.post('/', Books.create);
 
 module.exports = router;
